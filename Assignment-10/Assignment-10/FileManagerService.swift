5189d810ffcd854841e0ef07de95d087053a8d79
fenil292 <65783083+fenil292@users.noreply.github.com>
2021-12-13 15:59:57 +0530

    Update FileManagerService.swift

--- Assignment-10/Assignment-10/FileManagerService.swift
+++ Assignment-10/Assignment-10/FileManagerService.swift
@@ -1,30 +1,34 @@
 //
 //  FileManagerService.swift
-//  Assignment-10
+//  myFileManager
 //
-//  Created by DCS on 11/12/21.
+//  Created by DCS on 03/12/21.
 //  Copyright © 2021 DCS. All rights reserved.
 //
 
-import UIKit
-
-class FileManagerService: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+import Foundation
+class  FileManagerService {
+    static func getDocDir() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        print("Doc Dir: \(paths[0])")
+        return paths[0]
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    static func getfiles() -> Array<String> {
+        let fm = FileManager.default
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        var files = [String]()
+        do {
+            let items = try fm.contentsOfDirectory(atPath: path.path)
+            
+            for item in items {
+                //print("Found \(item)")
+                if item.contains(".txt") {
+                    files.append(item.components(separatedBy: ".").first ?? "")
+                }
+            }
+        } catch {
+            // failed to read directory – bad permissions, perhaps?
+        }
+        return files
     }
-    */
-
 }
